de91b8737801356c5df63e534de8f921735b2db6
panchalshubham465 <panchalshubham465@gmail.com>
2022-10-05 11:48:01 +0530

    first commit

---
+++ fontscript.js
@@ -0,0 +1,63 @@
+
+    function fun1(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f1";
+            }
+        
+            
+            function fun2(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f2";
+            }
+        
+            
+            
+            function fun3(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f3";
+            }
+        
+            
+            function fun4(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f4";
+            }
+            
+            function fun5(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f5";
+            }
+        
+            
+            function fun6(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f6";
+            }
+        
+        
+            function fun7(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f7";
+            }
+            
+            function fun8(){
+        document.getElementsByTagName("p")[0].style.fontFamily = "f8";
+            }
+            
+            
+            function fun9(){
+                
+                document.getElementsByTagName("p")[0].style.fontFamily = "f9";
+                    }
+                
+            
+            function fun10(){
+        
+        document.getElementsByTagName("p")[0].style.fontFamily = "Caveat";
+            }
+        
+            
+            function fun11(){
+        
+        document.getElementsByTagName("p")[0].style.fontFamily =  'Homemade Apple';
+            }
+        
+            
+            function fun12(){
+        
+        document.getElementsByTagName("p")[0].style.fontFamily =   'Liu Jian Mao Cao';
+            }
+        
---
+++ script.js
@@ -0,0 +1,27 @@
+
+function myFunction() {
+  var x = document.getElementById("myInput").value;
+document.getElementsByTagName("p")[0].innerHTML = x ;
+
+}
+  
+function setcol(){
+
+  var x =document.getElementById('clr').value;  
+
+document.getElementsByTagName("p")[0].style.color = x ;
+
+
+}
+
+
+
+  
+function text_b(){
+
+  var x =document.getElementById('boldd').value;  
+
+document.getElementsByTagName("p")[0].style.fontWeight ="bold";
+
+
+}
